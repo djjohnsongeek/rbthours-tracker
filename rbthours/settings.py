f602300fab7be81585f244adf82b9550be19380a
djjohnsongeek <danieleejohnson@gmail.com>
2019-10-04 09:38:33 -0400

    attempting to connect to Heroku postgres instead of SQLite

--- rbthours/settings.py
+++ rbthours/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,14 +75,8 @@ WSGI_APPLICATION = 'rbthours.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'rbthours.sqlite3',
-    }
-}
-
+import dj_database_url
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
