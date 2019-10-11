acc95128343b76aa8c936f3fe3e693236bdf2bd4
djjohnsongeek <danieleejohnson@gmail.com>
2019-10-11 14:43:34 -0400

    added strip_ids() helper function

--- tracker_app/helper.py
+++ tracker_app/helper.py
@@ -15,3 +15,17 @@ def convert_month(num:str):
         "July","August","September","October","November","December"
     ]
     return(months[num - 1])
+
+def strip_ids(list_of_dict:list):
+    """ 
+    Dictionary -> List of Integers remove all of the id values from a list of
+    dictionaries and return them in a list format
+    """
+    list_of_ids = []
+    for row in list_of_dict:
+        print(row)
+        list_of_ids.append(row["id"])
+        del row["id"]
+
+    return list_of_ids
+
