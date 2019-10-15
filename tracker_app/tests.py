690b4832d670ef69f7aaca46ae3857775ca05a88
djjohnsongeek <danieleejohnson@gmail.com>
2019-10-15 16:00:18 -0400

    added unit tests for convert_month helper funciton

--- tracker_app/tests.py
+++ tracker_app/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 from . import helper
+
 # Create your tests here
+
+# Test Modeles
+# Test Views
+
+# Test Custom Helper Functions
+class ConvertMonthTestCase(TestCase):
+    def test_month_is_correct(self):
+        """ Integer is properly converted to Month String """
+        self.assertEqual(helper.convert_month("1"), "January")
+        self.assertEqual(helper.convert_month(1), "January")
+        self.assertEqual(helper.convert_month("6"), "June")
+        self.assertEqual(helper.convert_month("12"), "December")
+        self.assertEqual(helper.convert_month(12), "December")
+    
+    def test_handles_non_numeric_args(self):
+        """ Returns valueError if a non numerical input is supplied """
+        with self.assertRaises(ValueError):
+            helper.convert_month("hellos")
+            helper.convert_month("23423.5")
+
+    def test_handles_large_numbers(self):
+        """ Returns ValueError if number is too big """
+        with self.assertRaises(ValueError):
+            helper.convert_month(23)
+            helper.convert_month("13")
+
+    def test_handles_negative_numbers(self):
+        """ Properly handles negative number arguments """
+        self.assertEqual(helper.convert_month(-5), "May")
+
+    def test_handles_decimal_args(self):
+        """ Properly rounds down on ALL float types"""
+        self.assertEqual(helper.convert_month(2.3), "Febuary")
+        self.assertEqual(helper.convert_month(2.9), "Febuary")
+
+    # class StripRowIdsFromListOfDict(TestCase):
+    #     def test_deletes_id_key_values
+    #     def test_handles_empty_list:
+    #     def test_handles_non_existent_key:
+    #     def test_returns_listOfint
